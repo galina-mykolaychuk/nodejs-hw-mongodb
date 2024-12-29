00a7db62c368fd3c8046f89b6dc81465e586e75a
Galina Mykolaychuk (Flawia) <gflawiae@gmail.com>
2024-12-29 23:27:53 +0200

    fix bugs and finish the project

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -8,13 +8,6 @@ const contactSchema = new mongoose.Schema(
     name: { type: String, required: true },
     phoneNumber: { type: String, required: true },
     email: { type: String },
-    isFavourite: { type: Boolean, default: false },
-    contactType: {
-      type: String,
-      enum: ['work', 'home', 'personal'],
-      required: true,
-      default: 'personal',
-    },
     // Додаємо поле для userId
     userId: {
       type: mongoose.Schema.Types.ObjectId,
--- src/validation/contactsSchemas.js
+++ src/validation/contactsSchemas.js
@@ -7,8 +7,6 @@ const contactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().optional().allow(null),
-  isFavourite: Joi.boolean().optional(),
-  contactType: Joi.string().valid('home', 'work', 'personal').required(),
   createdAt: Joi.date().default(() => new Date().toISOString()),
   updatedAt: Joi.date().default(() => new Date().toISOString()),
 });
@@ -18,8 +16,6 @@ const contactUpdateSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email().optional().allow(null),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('home', 'work', 'personal'),
   updatedAt: Joi.date().default(() => new Date().toISOString()),
 }).min(1);
 
